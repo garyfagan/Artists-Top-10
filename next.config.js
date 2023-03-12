9f8680545581e0bf18500e8df625bbdebc3ff43b
Gary Fagan <gary.fagan@workbuzz.com>
2023-03-12 15:32:42 +0000

    update env vars

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
-    SPOTIFY_AUTH_ENDPOINT: process.env.SPOTIFY_AUTH_ENDPOINT,
-    SPOTIFY_RESPONSE_TYPE: process.env.SPOTIFY_RESPONSE_TYPE,
     SPOTIFY_REDIRECT_URI: process.env.SPOTIFY_REDIRECT_URI,
   },
 }
--- src/components/LoginButton/LoginButton.tsx
+++ src/components/LoginButton/LoginButton.tsx
@@ -3,7 +3,7 @@ import { Button } from '@mui/material';
 
 const LoginButton: React.FC = () => (
   <Button
-    href={`${process.env.SPOTIFY_AUTH_ENDPOINT}?client_id=${process.env.SPOTIFY_CLIENT_ID}&redirect_uri=${process.env.SPOTIFY_REDIRECT_URI}&response_type=${process.env.SPOTIFY_RESPONSE_TYPE}`}
+    href={`https://accounts.spotify.com/authorize?client_id=256408cb74414ae4ab87f13514ae9d4d&redirect_uri=${process.env.SPOTIFY_REDIRECT_URI}&response_type=token`}
     variant="contained"
   >
     Login to Spotify
