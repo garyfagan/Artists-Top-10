e1761cf71c182b8446db453271c5dea7f9a936a6
Gary Fagan <gary.fagan@workbuzz.com>
2023-05-09 22:05:56 +0100

    update image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ const Home = () => {
         <title>Spotify Track Popularity</title>
       </Head>
       <Box sx={{ textAlign: 'center', margin: '100px auto 0', width: 250 }}>
-        <img src='/spotify-logo.png' alt='logo' width="140px" />
+        <img src='https://garyfagan.github.io/Spotify-Top-Ten/spotify-logo.png' alt='logo' width="140px" />
         <LoginButton />
       </Box>
     </>
